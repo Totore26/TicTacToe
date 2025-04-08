e5e5cb6c8c752db013c84c2d21abb964225512c8
Praise32 <marianosommella02@gmail.com>
2025-04-08 15:17:51 +0200

    Modifiche Client

--- IL VERO TRIS/Client/client.c
+++ IL VERO TRIS/Client/client.c
@@ -7,7 +7,20 @@ int main() {
     inizializza_socket();
     char buffer[1024];
     while (1) {
+        printf("In attesa di messaggi dal server...\n");
+        memset(buffer, 0, sizeof(buffer));
+        if (recv(sd, buffer, sizeof(buffer), 0) <= 0) {
+            printf("Connessione al server persa.\n");
+            exit(EXIT_FAILURE);
+        }
+        printf("Messaggio ricevuto: %s\n", buffer);
+        for (int i = 0; i < 5; i++)
+        {
+            printf("%d \n", i);
+            sleep(1);
+        }
         
+        // Controlla il messaggio ricevuto
         if (strstr(buffer, MSG_SERVER_MENU)) {
             funzione_menu();
         }
