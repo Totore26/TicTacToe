420fdd46cac93d54fb642cb7d093315eb16aedb9
999en <jakeventrone95@gmail.com>
2025-05-12 17:25:07 +0200

    risolto bug lista partite

--- tris-lso/Client/strutture.h
+++ tris-lso/Client/strutture.h
@@ -3,9 +3,9 @@
 #include "Comunicazione.h"
 
 //dimensioni buffer per leggere e scrivere sulla socket
-#define MAXLETTORE 1024
+#define MAXLETTORE 2048
 #define MAXSCRITTORE 16
-#define BUFFER_SIZE 1024 
+#define BUFFER_SIZE 2048 
 
 //serve alla funzione partita per gestire gli input
 enum tipo_giocatore
